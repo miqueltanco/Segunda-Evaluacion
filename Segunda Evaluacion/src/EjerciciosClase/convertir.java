07270056ae587261fe95cd2200d510ecc9452392
alumne-DAM <alumne-DAM@172.16.26.139>
2019-01-07 19:11:30 +0100

    tarde

---
+++ Segunda Evaluacion/src/EjerciciosClase/convertir.java
@@ -0,0 +1,42 @@
+package EjerciciosClase;
+
+public class convertir {
+	
+	public int conversion(char numero) {
+		
+			switch (numero) {
+				case 48:
+					numero=0;
+					break;
+				case 49:
+					numero=1;
+					break;
+				case 50:
+					numero=2;
+					break;
+				case 51:
+					numero=3;
+					break;
+				case 52:
+					numero=4;
+					break;
+				case 53:
+					numero=5;
+					break;
+				case 54:
+					numero=6;
+					break;
+				case 55:
+					numero=7;
+					break;
+				case 56:
+					numero=8;
+					break;
+				case 57:
+					numero=9;
+					break;
+			}			
+		return numero;
+	}
+}
+
