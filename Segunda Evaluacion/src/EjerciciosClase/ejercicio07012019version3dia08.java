eef73be31dc492da3cef73db650f701d815960c9
alumne-DAM <alumne-DAM@172.16.26.139>
2019-01-08 17:07:53 +0100

    subido version3

---
+++ Segunda Evaluacion/src/EjerciciosClase/ejercicio07012019version3dia08.java
@@ -0,0 +1,17 @@
+package EjerciciosClase;
+
+import java.util.Scanner;
+
+public class ejercicio07012019version3dia08 {
+
+	public static void main (String[]args) {
+	
+		Scanner sc = new Scanner(System.in);
+		int numero=sc.nextInt();
+				
+		for (int i=0; numero>i; i++) {
+			numero=Integer.parseInt("0");			
+		}
+		
+	}
+}
