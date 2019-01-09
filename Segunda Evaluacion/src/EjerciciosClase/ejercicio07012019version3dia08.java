c4ca554d2bfd5462829326668cba008efc5792dd
Yugen <Yugen@192.168.0.25>
2019-01-09 01:02:56 +0100

    09-01-19 1:09 am

--- Segunda Evaluacion/src/EjerciciosClase/ejercicio07012019version3dia08.java
+++ Segunda Evaluacion/src/EjerciciosClase/ejercicio07012019version3dia08.java
@@ -7,11 +7,28 @@ public class ejercicio07012019version3dia08 {
 	public static void main (String[]args) {
 	
 		Scanner sc = new Scanner(System.in);
-		int numero=sc.nextInt();
-				
-		for (int i=0; numero>i; i++) {
-			numero=Integer.parseInt("0");			
-		}
-		
+		 int n = sc.nextInt();
+		 int l = Integer.toString(n).length();
+		 int s = 0;
+		 
+		 while(n>=0) {
+		 
+		 for (int i = 0; i < l; i++) {
+			 String num = String.valueOf(Integer.toString(n).charAt(i));
+			 s+=Integer.parseInt(num);
+			 
+			 System.out.print(num);
+						 
+			 if(!(i==l-1)) {
+					System.out.print(" + ");
+				}
+			 
+		 }
+		 System.out.println(" = " + s); 
+		 
+		 n = sc.nextInt();
+		 l = Integer.toString(n).length();
+		 s = 0;
+		 }
 	}
 }
